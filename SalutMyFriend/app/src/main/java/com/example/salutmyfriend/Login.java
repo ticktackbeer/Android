079d643f8a0b297f3830c0c91f8ad693a982af99
Edu <Edu>
2020-11-29 16:40:50 +0100

    Edu Projekt

---
+++ SalutMyFriend/app/src/main/java/com/example/salutmyfriend/Login.java
@@ -0,0 +1,33 @@
+package com.example.salutmyfriend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    Button callSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //This Line will hide the status bar from the screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+        callSignUp = findViewById(R.id.signup_screen);
+
+        callSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (Login.this,SignUp.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
