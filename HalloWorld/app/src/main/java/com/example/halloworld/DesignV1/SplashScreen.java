d7a2689601c38da9f4339b3e98962a6b2dbf0804
Niat <TICTEST@GMAIL.COM>
2021-01-04 22:45:18 +0100

    code verbindungen von SplashScreen bis anmeldeauswahl

--- HalloWorld/app/src/main/java/com/example/halloworld/DesignV1/SplashScreen.java
+++ HalloWorld/app/src/main/java/com/example/halloworld/DesignV1/SplashScreen.java
@@ -2,15 +2,65 @@ package com.example.halloworld.DesignV1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 import com.example.halloworld.R;
+import static android.os.Build.VERSION.SDK_INT;
 
 public class SplashScreen extends AppCompatActivity {
+
+    //Variablen
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
+    private static int SPLASH_SCREEN = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //Hooks
+        image = findViewById(R.id.image_Splash);
+        logo = findViewById(R.id.topic_Splash);
+        slogan = findViewById(R.id.text_Motto);
+
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Wilkommen.class);
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(image, "logo_image");
+                pairs[1] = new Pair<View, String>(logo, "logo_text");
+                if (SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this, pairs);
+                    startActivity(intent, options.toBundle());
+                    finish();
+                }
+            }
+        }, SPLASH_SCREEN);
+
     }
 }
--- HalloWorld/app/src/main/java/com/example/halloworld/DesignV1/Wilkommen.java
+++ HalloWorld/app/src/main/java/com/example/halloworld/DesignV1/Wilkommen.java
@@ -2,15 +2,31 @@ package com.example.halloworld.DesignV1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.halloworld.R;
 
 public class Wilkommen extends AppCompatActivity {
 
+    Button fortsetzen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wilkommen);
+
+        fortsetzen = findViewById(R.id.button_Fortsetzen);
+
+        fortsetzen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Wilkommen.this, Anmeldeauswahl.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
